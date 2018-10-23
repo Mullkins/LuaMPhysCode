05fe26774c08bec1a8ca8015a71cc897cd777315
Mullkins <44404870+Mullkins@users.noreply.github.com>
2018-10-23 15:16:21 +0100

    Batch file
    The batch file for setup of auto-contouring.

---
+++ Batch_GentleProstate_2atlases.bat
@@ -0,0 +1,8 @@
+cd..
+cd..
+c:
+cd c:\ProgramData\ADMIRE
+path "c:\Program Files\ADMIRE"
+
+ADMIRE.exe --type gen --patient D:\data\ADMIRE2WMatch\inter\tmpDataAlexAndTom --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt1CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt2CT --results D:\data\ADMIRE2WMatch\inter\ResultsAlexAndTom --rflabel-fusion --gpu
+pause
