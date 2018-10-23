9d0b64ddb944fdd27d048fd83a497ed7d19ed5a2
Mullkins <44404870+Mullkins@users.noreply.github.com>
2018-10-23 17:44:34 +0100

    Update Batch_GentleProstate_2atlases.bat
    change for all atlases and removed pause so iteration continue without external input

--- Batch_GentleProstate_2atlases.bat
+++ Batch_GentleProstate_2atlases.bat
@@ -4,5 +4,4 @@ c:
 cd c:\ProgramData\ADMIRE
 path "c:\Program Files\ADMIRE"
 
-ADMIRE.exe --type gen --patient D:\data\ADMIRE2WMatch\inter\tmpDataAlexAndTom --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt1CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt2CT --results D:\data\ADMIRE2WMatch\inter\ResultsAlexAndTom --rflabel-fusion --gpu
-pause
+ADMIRE.exe --type gen --patient D:\data\ADMIRE2WMatch\inter\tmpDataAlexAndTom --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt1CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt2CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt3CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt4CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt5CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt6CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt7CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt8CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt9CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt10CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt11CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt12CT --atlas D:\data\ADMIRE2WMatch\inter\Atlases\GentleProstate\pt13CT --results D:\data\ADMIRE2WMatch\inter\ResultsAlexAndTom --rflabel-fusion --gpu
